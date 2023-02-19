8694681c46e74dcd991dad81c74ae0f3210f8142
MossaJehad <Moosa.Jehad65@gmail.com>
2023-02-19 19:49:35 +0300

    Search Widget

---
+++ app.js
@@ -0,0 +1,13 @@
+const search = document.querySelector('.search')
+const btn = document.querySelector('.btn')
+const input = document.querySelector('.input')
+
+btn.addEventListener('click', () => {
+    if(search.classList.contains('active')){
+        search.classList.remove('active')
+    } else {
+        search.classList.add('active')
+        input.focus()
+    }
+})
+
